4ba1437daf8160c1a0b7dd875287c4157938afe2
jung410 <43362052+jung410@users.noreply.github.com>
2020-04-26 00:38:30 +0900

    Update MemberServiceTest.java

--- YouAndMeDiary/src/test/java/com/diary/YouAndMeDiary/service/MemberServiceTest.java
+++ YouAndMeDiary/src/test/java/com/diary/YouAndMeDiary/service/MemberServiceTest.java
@@ -1,36 +1,36 @@
-package com.diary.YouAndMeDiary.service;
+// package com.diary.YouAndMeDiary.service;
 
-import com.diary.YouAndMeDiary.domain.Member;
-import com.diary.YouAndMeDiary.repository.MemberRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
+// import com.diary.YouAndMeDiary.domain.Member;
+// import com.diary.YouAndMeDiary.repository.MemberRepository;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
+// import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
+// import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Transactional
-public class MemberServiceTest {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// @Transactional
+// public class MemberServiceTest {
 
-    @Autowired
-    MemberRepository memberRepository;
-    @Autowired
-    MemberService memberService;
+//     @Autowired
+//     MemberRepository memberRepository;
+//     @Autowired
+//     MemberService memberService;
 
-//     @Test
-//     public void 회원가입() throws Exception {
-//         Member member = new Member();
-//         member.setUserId("test1");
-//         member.setUserName("테스터");
-//         member.setUserEmail("test@test.test");
-//         member.setUserPassword("123123");
+// //     @Test
+// //     public void 회원가입() throws Exception {
+// //         Member member = new Member();
+// //         member.setUserId("test1");
+// //         member.setUserName("테스터");
+// //         member.setUserEmail("test@test.test");
+// //         member.setUserPassword("123123");
 
-//         Long saveId = memberService.join(member);
+// //         Long saveId = memberService.join(member);
 
-//         assertEquals(member, memberRepository.findOne(saveId));
-//     }
-}
+// //         assertEquals(member, memberRepository.findOne(saveId));
+// //     }
+// }
